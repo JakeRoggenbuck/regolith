61e8c4791fa5414ba200f3427778a79107cda40f
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-06-25 19:12:37 -0700

    Add first working version

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,49 @@
 #![deny(clippy::all)]
+use regex::Regex;
 
 #[macro_use]
 extern crate napi_derive;
 
 #[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
+pub struct Regolith {
+  regex: Option<Regex>,
+}
+
+#[napi]
+impl Regolith {
+  #[napi(constructor)]
+  pub fn new() -> Self {
+    Self {
+      regex: None,
+    }
+  }
+
+  #[napi]
+  pub fn do_match(&self, text: String, pattern: String) -> bool {
+    if let Ok(re) = Regex::new(&pattern) {
+      re.is_match(&text)
+    } else {
+      false
+    }
+  }
+
+  #[napi]
+  pub fn compile(&mut self, pattern: String) -> bool {
+    match Regex::new(&pattern) {
+      Ok(re) => {
+        self.regex = Some(re);
+        true
+      }
+      Err(_) => false,
+    }
+  }
+
+  #[napi]
+  pub fn test(&self, text: String) -> bool {
+    if let Some(ref re) = self.regex {
+      re.is_match(&text)
+    } else {
+      false
+    }
+  }
 }
