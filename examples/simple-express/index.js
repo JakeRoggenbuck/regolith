2e093352ce5c8c6c8cd56f7c0ed7a24b8ecde5be
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-07-11 15:24:34 -0700

    Add basic example of express

---
+++ examples/simple-express/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { Regolith } from '@regolithjs/regolith';
+
+const app = express();
+const port = 3000;
+
+const intPattern = new Regolith('^\\d+$');
+const floatPattern = new Regolith('^\\d*\\.\\d+$');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/check', (req, res) => {
+    const value = req.query.value;
+
+    if (!value) {
+        return res.status(400).send('Please provide a value query parameter');
+    }
+
+    const isInt = intPattern.test(value);
+    const isFloat = floatPattern.test(value);
+
+    res.json({
+        value,
+        isInt,
+        isFloat,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
